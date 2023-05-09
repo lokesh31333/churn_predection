c9ec440ef9857b9252c41090e545536c02d01af7
karysutariya <karysutariya1234@gmail.com>
2023-05-09 15:29:56 -0400

    big changes

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/kary/Desktop/upwork project/churn_predection/env"
+set -gx VIRTUAL_ENV "/home/kary/Desktop/upwork project/churn_predection/env2"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -52,8 +52,8 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Prompt override?
-        if test -n "(env) "
-            printf "%s%s" "(env) " (set_color normal)
+        if test -n "(env2) "
+            printf "%s%s" "(env2) " (set_color normal)
         else
             # ...Otherwise, prepend env
             set -l _checkbase (basename "$VIRTUAL_ENV")
